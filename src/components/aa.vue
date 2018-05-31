5a5324ec22fe418e5bad916fbc882e3f04be489e
Bruse zero <zero8297183@163.com>
2018-05-31 20:49:37 +0800

    修改图片上传插件

--- src/components/aa.vue
+++ src/components/aa.vue
@@ -3,11 +3,15 @@
     <p>{{msg}}</p>
     <h3>这是aac的儿子__aa</h3>
     <form action="" id='form'>
-        <input type="file" id='file' multiple="">
+        <input type="file" id='file' multiple="" @click='upfile($event)'>
         <div id='picShow'>
           <span v-for='p in picshowList'>
-            <span class="delPic" @click.native='delPic'>X</span>
-            <img src="p.src" alt="">
+            <span 
+              class="delPic"
+              :time='p.time'
+              @click='delpic($event)'
+              >X</span>
+            <img :src="p.src" alt="">
           </span>
         </div>
     </form>
@@ -27,49 +31,88 @@ export default {
   mounted () {
     var vm = this;
     console.log('upimg: ',upImg);
-    var file = document.querySelector('#file');
-    file.onclick = function(){
-        var _this = this;
+    // var file = document.querySelector('#file');
+    // file.onclick = function(){
+    //   var _this = this;
+    //   console.log('读取中...');
+    //   // 显示loading 
+    //   vm.$vux.loading.show({
+    //     text: '读取中...'
+    //   })
+    //   // 读取图片
+    //   upImg( _this, function(){
+    //     // 压缩提示(不压缩不进来)
+    //     console.log('图片压缩中...');
+    //     vm.$vux.loading.show({
+    //       text: '压缩中...'
+    //     })
 
-        // 读取图片
-        upImg( _this, function(){
-            // 压缩提示(不压缩不进来)
-            console.log('图片加载中...');
-
-        }, function(res){
-            console.log('res: ',res);
-            
-            if(res.src){
-                vm.picshowList.src = res.src;
-                console.log('vm.picshowList: ',vm.picshowList);
-            }
-        })
-    }
+    //   }, function(res){
+    //     console.log('res: ',res);
+    //     // binsrc: 用于上传到服务器
+    //     // src: 用于图片预览
+    //     if(res.src){
+    //       vm.picshowList.push({
+    //         src: res.src,
+    //         index: vm.picshowList.length
+    //       })
+    //     }
+       
+    //   })
+    // }
 
   },
   methods: {
-    // upfile (e) {
+    upfile (e) {
+      let vm = this;
+      let _this = e.currentTarget;
+      console.log('读取中...');
+      // 显示loading 
+      vm.$vux.loading.show({
+        text: '读取中...'
+      })
+      // 读取图片
+      upImg( _this, function(){
+        // 压缩提示(不压缩不进来)
+        console.log('图片压缩中...');
+        vm.$vux.loading.show({
+          text: '压缩中...'
+        })
 
-    //     // 读取图片
-    //     upImg( e.currentTarget, function(){
-    //         // 压缩提示(不压缩不进来)
-    //         console.log('图片加载中...');
-
-    //     }, function(res){
-    //         console.log('res: ',res);
-            
-    //         if(res.src){
-    //             picshowList.src = res.src;
-
-    //             img.src = res.src;      // 显示图片
-    //             span.appendChild(img);
-    //             picShow.appendChild(span);
-    //             // console.log(formdata.getAll('UploadForm[]'));
-    //         }
-    //     })
-    // },
+      }, function(res){
+        console.log('res: ',res);
+        // binsrc: 用于上传到服务器
+        // src: 用于图片预览
+        if(res.src){
+          vm.picshowList.push({
+            src: res.src,
+            time: new Date().getTime()
+          })
+        }
+       
+      })
+      setTimeout(function() {
+        vm.$vux.loading.hide()
+      },0);
+    },
     delpic (img) {
-      console.log('img: ',img);
+      let vm = this;
+      img = img.currentTarget;
+      let time = img.getAttribute('time');
+      let imgPar = img.parentNode;
+      // console.log('img: ',img);
+      // console.log('imgPar: ',imgPar);
+      // 移除元素
+      // imgPar.parentNode.removeChild(imgPar); 
+      for(var i=0; i<vm.picshowList.length; i++){
+        if(vm.picshowList[i].time == time){
+          console.log('time: ',time);
+          console.log('vm.picshowList[i]: ',vm.picshowList[i]);
+          // 删除数据
+          vm.picshowList.splice(i, 1);
+        }
+      }
+      console.log('vm.picshowList: ',vm.picshowList);
     }
   }
 }
