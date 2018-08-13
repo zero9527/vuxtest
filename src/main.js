a8ca208512ba44d07f260e48651fd18e48702348
zero9527 <zero8297183@163.com>
2018-08-13 22:08:41 +0800

    独立router

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 import App from './App'
-import Vux from './components/HelloFromVux'
 // 事件总线
 import Bus from './eventbus'
-import vuex1 from './components/vuex1'
 
 import store from './assets/js/store'
+
+import router from './router'
 
 import axios from 'axios'
 import { LoadingPlugin, XButton, Toast, XInput, Group } from 'vux'
@@ -19,54 +19,8 @@ Vue.component('x-input', XInput)
 Vue.component('group', Group)
 Vue.component('toast', Toast)
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 Vue.use(LoadingPlugin)
- 
-const routes = [
-	{
-	  path: '/',
-	  name: 'Vux',
-	  component: Vux
-	},
-	{
-		path: '/vuex1',
-		name: 'vuex1',
-		component: vuex1
-	},
-	{
-		path: '/es6',
-		name: 'es6',
-		component: resolve => require(['@/components/es6'],resolve)
-	},
-	{
-		path: '/bbb',
-		name: 'bbb',
-		component: resolve => require(['@/components/bbb'], resolve)
-	},
-	{
-		path: '/aac',
-		name: 'aac',
-		component: resolve => require(['@/components/aac'],resolve),
-		children: [
-			{
-				path: 'cc',
-				name: 'cc',
-				component: resolve => require(['@/components/cc'],resolve)
-			}
-		]
-	},
-	{	// 假装是aac的儿子
-		path: '/aac/list_state',
-		component: resolve => require(['@/components/list_state'],resolve),
-	},
-	{
-		path: '/list_state/list_detail',
-		component: resolve => require(['@/components/list_detail'],resolve)
-	}
-]
-const router = new VueRouter({
-  routes
-})
 
 FastClick.attach(document.body)
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,56 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import App from '../App'
+import Vux from '../components/HelloFromVux'
+// 事件总线
+import Bus from '../eventbus'
+import vuex1 from '../components/vuex1'
 Vue.use(Router)
+ 
+const routes = [
+	{
+	  path: '/',
+	  name: 'Vux',
+	  component: Vux
+	},
+	{
+		path: '/vuex1',
+		name: 'vuex1',
+		component: vuex1
+	},
+	{
+		path: '/es6',
+		name: 'es6',
+		component: resolve => require(['@/components/es6'],resolve)
+	},
+	{
+		path: '/bbb',
+		name: 'bbb',
+		component: resolve => require(['@/components/bbb'], resolve)
+	},
+	{
+		path: '/aac',
+		name: 'aac',
+		component: resolve => require(['@/components/aac'],resolve),
+		children: [
+			{
+				path: 'cc',
+				name: 'cc',
+				component: resolve => require(['@/components/cc'],resolve)
+			}
+		]
+	},
+	{	// 假装是aac的儿子
+		path: '/aac/list_state',
+		component: resolve => require(['@/components/list_state'],resolve),
+	},
+	{
+		path: '/list_state/list_detail',
+		component: resolve => require(['@/components/list_detail'],resolve)
+	}
+]
 
 export default new Router({
-  routes: [
-    {
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes
 })
